4f96faa9c6ec6219ca7a2eaa6d8401b357978994
Conny González <119901329+sparklyeonn@users.noreply.github.com>
2025-11-19 22:20:39 -0300

    first commit

---
+++ src/main/java/cl/ritmolab/ritmolab_backend/RitmolabBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.ritmolab.ritmolab_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RitmolabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RitmolabBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ritmolab/ritmolab_backend/RitmolabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ritmolab.ritmolab_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RitmolabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
